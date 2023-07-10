bdeb8b5c91e3f891214981f2ab01f78f5d189874
Wilson Valente <wilsonv@ciandt.com>
2023-07-10 00:26:05 -0300

    Start project with Angular 16

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AQT_Frontend_Atividade01_5';
+}
